ef403ed2777ba34b5034f94f96ecca6d50f095c6
erebus1 <erebus@mail.ua>
2015-04-09 10:50:47 +0300

    Interfeces

---
+++ ILocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BigBrother
+{
+    interface Location
+    {
+        
+        Dictionary<String, float> getLocation(String profileLink);
+        
+    }
+}
